05b469c309cc7f3324f879282978f46886652a30
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-01-15 19:30:31 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string korisnik = Console.ReadLine();
+            string[] polje = korisnik.Split(' ');
+            Console.WriteLine(polje[0]+polje[polje.Length-1]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
